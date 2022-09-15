71c6f2aa4ab542e46c9d9a705375d63277e9dc4e
Tyler Truman <trucotyler@gmail.com>
2022-09-15 14:31:17 -0600

    setup

--- AllSpice.client/src/env.js
+++ AllSpice.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'http://localhost:5001' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'tylertruman.us.auth0.com'
+export const clientId = 'aYponvB5txjq1f3GfcMZUCkAzGnHNdDP'
+export const audience = 'https://myapi.com'
--- AllSpice/dbSetup.sql
+++ AllSpice/dbSetup.sql
@@ -6,3 +6,30 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+
+CREATE TABLE IF NOT EXISTS recipes(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  subtitle VARCHAR(255),
+  img VARCHAR(255),
+  category VARCHAR(255) NOT NULL,
+  creatorId VARCHAR(255) NOT NULL,
+  FOREIGN KEY (creatorId) REFERENCES accounts(id)
+) default charset utf8 COMMENT '';
+
+-- NOTE deletes the table and all of its content.
+DROP TABLE recipes;
+
+-- NOTE creates new data entry
+INSERT INTO recipes(title, subtitle, img, category)
+VALUES('Spooky Cupcakes', 'Now made with Gluten!', 'https://images.unsplash.com/photo-1638258682206-0a95e15e4030?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fGhhbGxvd2VlbiUyMGN1cGNha2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60', 'Dessert');
+
+-- NOTE join tables (recipes and accounts)
+SELECT * FROM recipes r
+JOIN accounts a ON a.id = r.creatorId;
+
+-- NOTE join tables (recipes, ingredients, steps, and accounts)
+SELECT * FROM recipes r
+JOIN ingredients i ON r.id = i.recipeId
+JOIN steps s ON r.id = s.recipeId
+JOIN accounts a ON a.id = r.creatorId;
